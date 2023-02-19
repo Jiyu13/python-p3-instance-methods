ec462d67b5e823e119cb27179a716915d1f82efb
Ziru Zou <jiyucoding@yahoo.com>
2023-02-19 17:50:22 -0500

    add instance snoop, create sit() in class Dog and call it on instances

--- lib/dog.py
+++ lib/dog.py
@@ -3,11 +3,25 @@
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    # Instance method definition
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
 
 
 fido = Dog()
+snoopy = Dog()
 
 # access instance's __dir__() method with dot notation
 fido.__dir__()
+
+# call bark() on instances
+fido.bark()
+snoopy.bark()
+
+
+# call sit() on instances
+fido.sit()
+snoopy.sit()
